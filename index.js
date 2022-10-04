d1bab40408ac91cdff4b05b16123e2ee52150070
jvcataquiz <jvcataquiz@gmail.com>
2022-10-04 16:36:06 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//using ejs package
+app.set("view engine","ejs");
+// path for css ,js , image 
+app.use(express.static(path.join(__dirname, '/public')));
+//path for templating, this is for the pages
+app.set('views',path.join(__dirname, '/views'));
+
+
+
+
+
+//this is the port where we can listen, port: 8080
+app.listen(8080, () => {
+    console.log("listening on port 8080");
+})
