6483c1a81ddc03f4fa2b2815fe02c6d0fa5fa191
jvcataquiz <jvcataquiz@gmail.com>
2022-10-06 19:44:10 +0800

    adding google api

--- index.js
+++ index.js
@@ -2,32 +2,56 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const file = require('./data.json');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //using ejs package
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 // path for css ,js , image 
 app.use(express.static(path.join(__dirname, '/public')));
 //path for templating, this is for the pages
-app.set('views',path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/views'));
 
 
-const session = true ;
+const session = true;
 
 //route for the homepage
-app.get('/', (req, res)=>{
-    res.render('index', {session,file});
-   })
+app.get('/', (req, res) => {
+    res.render('index', { session, file });
+})
+
+app.get('/try', (req, res) => {
+    res.render('try');
+})
+
 
 //route for the rental page
-   app.get('/rental/:id', (req, res)=>{
-    const {id}  = req.params;
-    if(session){
-        res.render('rental', {session,id});
+app.get('/rental/:id', (req, res) => {
+    const { id } = req.params;
+    const lat =  14.5243;
+    const long = 121.0792;
+    if (session) {
+       
+        res.render('rental', { session, id , lat, long});
     }
-    else{
+    else {
         res.redirect('/');
     }
-   
-   })
+
+})
+
+
+
+//route for the profile page
+app.get('/profile', (req, res) => {
+    if (session) {
+        res.render('profile', { session });
+    }
+    else {
+        res.redirect('/');
+    }
+
+})
 
 
 //this is the port where we can listen, port: 8080
