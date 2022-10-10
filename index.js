68db160d3fdb3f6ebf0c20cbbdd780c3b3c3ed33
jvcataquiz <jvcataquiz@gmail.com>
2022-10-10 16:21:30 +0800

    added moongose, working signup form , working sign in with nosql

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const file = require('./data.json');
+const mongoose = require('mongoose');
+const { Sign } = require('crypto');
+
+//to deserialize json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+//connection to open mongooose
+mongoose.connect('mongodb://localhost:27017/test')
+    .then(() => {
+        console.log("Connection Open");
+    })
+    .catch(err => {
+        console.log("error");
+        console.log(err);
+    });
+
+
+
+
+
+
+
+
 
 //using ejs package
 app.set("view engine", "ejs");
@@ -12,9 +35,38 @@ app.use(express.static(path.join(__dirname, '/public')));
 //path for templating, this is for the pages
 app.set('views', path.join(__dirname, '/views'));
 
-//session
-const session = true;
-// fake id
+
+const signUpSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    pwd: {
+        type: String,
+        require: true
+    }
+})
+const Signup = mongoose.model('Signup', signUpSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fake session
+let session = false;
+let user_id ; 
 
 //route for the homepage
 app.get('/', (req, res) => {
@@ -28,8 +80,8 @@ app.get('/try', (req, res) => {
 
 //route for the rental page // need id only to find the id from the database //
 app.get('/rental/:address/:lat/:long/:id', (req, res) => {
-   const{address, lat, long, id} = req.params;
-    res.render('rental',{ session, address, lat, long, id});
+    const { address, lat, long, id } = req.params;
+    res.render('rental', { session, address, lat, long, id });
 })
 
 //added new apartment
@@ -40,7 +92,7 @@ app.post('/rental', (req, res) => {
 //route for the profile page
 app.get('/profile', (req, res) => {
     if (session) {
-        res.render('profile', { session });
+        res.render('profile', { session , user_id});
     }
     else {
         res.redirect('/');
@@ -58,6 +110,54 @@ app.get('/post', (req, res) => {
     }
 
 })
+
+
+
+//added new user (Sign up form)
+app.post('/signup', (req, res) => {
+    const { fullname, email, pwd } = req.body;
+    Signup.findOne({ email: email })
+        .then(result => {
+            if (result === null) {
+                const newuser = new Signup({
+                    fullname: fullname,
+                    email: email,
+                    pwd: pwd
+                });
+                newuser.save()
+                    .then(data => {
+                        console.log("Successfully Created a new user");
+                        console.log(data);
+                        user_id  = newuser.id;
+                        session = true;
+                        res.redirect('/');
+                    })
+                    .catch(err => {
+                        console.log("Failed to enter new user");
+                        console.log(err);
+                        session = false;
+                    })
+            }
+            else {
+                res.send("Email Data Found")
+            }
+        })
+})
+
+//Sign in Fomm
+app.post('/signin', (req, res) => {
+    const { sign_email, sign_pwd } = req.body;
+    Signup.find({$and:[{ email: sign_email ,  pwd: sign_pwd}]})
+        .then(result => {
+            if (result.length != 0) {
+               console.log("Welcome !!");
+            }
+            else {
+                res.send("no data found")
+            }
+        })
+})
+
 
 //this is the port where we can listen, port: 8080
 app.listen(8080, () => {
